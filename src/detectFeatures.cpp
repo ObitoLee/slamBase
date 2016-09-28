2ce9e1061925cadb7201d1ec7d7286dee2f62dc9
Allan Lee <534007689@qq.com>
2016-09-28 09:23:25 +0800

    精简detectFeatures.cpp

--- src/detectFeatures.cpp
+++ src/detectFeatures.cpp
@@ -19,22 +19,17 @@ CAMERA_INTRINSIC_PARAMETERS C={	atof( cfg.getData( "camera.cx" ).c_str() ),
 								atof( cfg.getData( "camera.fy" ).c_str() ),
 								atof( cfg.getData( "camera.scale" ).c_str() )};
 double goodMatchThreshold = atof( cfg.getData( "good_match_threshold" ).c_str() );
-string detector=cfg.getData( "detector" ).c_str();
-string descriptor=cfg.getData( "descriptor" ).c_str();
+string detector = cfg.getData("detector");
+string descriptor = cfg.getData("descriptor");
 
 int main()
 {
     // 声明并从data文件夹里读取两个rgb与深度图
-    Mat rgb1 = imread( "./data/rgb1.png");
-    Mat rgb2 = imread( "./data/rgb2.png");
-    Mat depth1 = imread( "./data/depth1.png", -1);
-    Mat depth2 = imread( "./data/depth2.png", -1);
-
 	FRAME frame1,frame2;
-	frame1.rgb=rgb1;
-	frame1.depth=depth1;
-	frame2.rgb=rgb2;
-	frame2.depth=depth2;
+    frame1.rgb = imread( "./data/rgb1.png");
+    frame2.rgb = imread( "./data/rgb2.png");
+    frame1.depth = imread( "./data/depth1.png", -1);
+    frame2.depth = imread( "./data/depth2.png", -1);
 
 	computeKeyPointsAndDesp(frame1,detector,descriptor);
     computeKeyPointsAndDesp(frame2,detector,descriptor);
